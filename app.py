7eb7cb3e46bee0cc3bd439a402bb29506ad5f690
Gabriel <gabrielpinheiro632@gmail.com>
2025-06-21 16:58:56 -0300

    Adicionado validação regex para o login, além de organizar o css

--- app.py
+++ app.py
@@ -1,31 +1,45 @@
 from flask import Flask, request, render_template, flash, redirect, url_for
+import re
 
 app = Flask(__name__)
 
-app.secret_key = 'reader632'
+app.secret_key = 'Reader632' # chave para 'flash' funcionar e garantir segurança ao Flask
+
+
+def usuario_valido(usuario):
+    return re.fullmatch(r'^[a-zA-Z0-9_]{3,20}$', usuario) is not None
+
+def senha_valida(senha):
+    # Pelo menos 6 caracteres, pelo menos uma letra e um número
+    return re.fullmatch(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$', senha) is not None
+
 
 @app.route('/', methods=['GET', 'POST'])
 def tela_login():
     if request.method == 'POST':
+        # recuperando dados com base no 'name' do HTML
         usuario = request.form.get('usuario', '').strip()
         senha = request.form.get('senha', '').strip()
         erro = False
 
-        if len(usuario) < 3:
-            flash('* O nome de usuário deve ter pelo menos 3 caracteres.')
+        if not usuario_valido(usuario):
+            flash('* O usuário deve ter apenas letras e números (3-20 caracteres).')
             erro = True
-        if len(senha) < 6:
-            flash('* A senha deve ter pelo menos 6 caracteres.')
+
+        if not senha_valida(senha):
+            flash('* A senha deve ter pelo menos 6 caracteres, incluindo letras e números.')
             erro = True
 
         if erro:
             return redirect(url_for('tela_login'))
         else:
             flash('Login bem-sucedido!')
+            # trocar pela página que será direcionada ao fazer login
             return redirect(url_for('tela_login'))
 
     return render_template('index.html')
-        
+
+
 
 
 
